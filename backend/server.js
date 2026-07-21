3a593f9d0ea11b845d0f7b42a49ea2fda732f51a
Sivanishanth <Sivanishanth303@gmail.com>
2026-07-22 00:04:28 +0530

    Connected backend to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const { MongoMemoryServer } = require('mongodb-memory-server')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-app.use(cors())
-app.use(express.json({ limit: '50mb' }))
+const app = express();
 
-app.use('/api/logs', require('./routes/logs'))
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
-const PORT = 5000
+app.use("/api/logs", require("./routes/logs"));
+
+const PORT = process.env.PORT || 5000;
 
 async function startServer() {
-  const mongod = await MongoMemoryServer.create()
-  const uri = mongod.getUri()
-  
-  await mongoose.connect(uri)
-  console.log('MongoDB Memory Server connected')
-  
-  app.listen(PORT, () => console.log('Server running on port ' + PORT))
+  try {
+    console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("✅ MongoDB Atlas connected");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error);
+  }
 }
 
-startServer()
+startServer();
